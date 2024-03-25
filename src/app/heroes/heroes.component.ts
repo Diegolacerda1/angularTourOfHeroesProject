37667e3dd93cc6c662b6b263ea84f53b4bd13c36
Diegolacerda1 <128429943+Diegolacerda1@users.noreply.github.com>
2024-03-25 11:06:57 -0400

    service updated

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 import {/* . . . */
 NgFor,
 /* . . . */
@@ -11,9 +11,19 @@ NgFor,
   styleUrl: './heroes.component.css',
 })
 export class HeroesComponent {
-  heroes = HEROES; 
+  heroes: Hero[] = []; 
   selectedHero?: Hero;
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+  }
+
+  constructor(private heroService: HeroService) {}
+
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
+  }
+
+  ngOnInit(): void {
+    this.getHeroes();
   }
 }
