e66796b341453d2462b70f7dfe251e84b468e9f2
Diegolacerda1 <128429943+Diegolacerda1@users.noreply.github.com>
2024-03-25 11:41:49 -0400

    step 4 done

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 import {/* . . . */
 NgFor,
 /* . . . */
